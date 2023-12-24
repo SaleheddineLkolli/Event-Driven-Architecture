8bb278a78aad906868760a490164ad4ea3021844
saleheddinelkolli <s.lkolli@etu.enset-media.ac.ma>
2023-12-24 04:43:52 +0100

    add

---
+++ src/test/java/org/example/eventdrivenarchitecture/EventDrivenArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.eventdrivenarchitecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventDrivenArchitectureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
